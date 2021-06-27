94561b98a050bb4372fd1867572317bdfb6b3531
NC <nchoglec@gmail.com>
2021-06-27 00:26:56 -0400

    Latest Update
    Mostly Finished

--- script.js
+++ script.js
@@ -5,13 +5,43 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
 
+    //Scroll Up Script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    //Scroll to Element
+    
+    $("a[href^='#']").on('click', function(e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 60
+        }, 300, function(){
+            window.location.hash = hash;
+        });
+    });
+     
 
     //toggle menu-navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    //Typing Animation
+    var typed = new Typed(".typing", {
+        strings: ["Software/Hardware Engineer", "CPU/GPU Architect"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
     
 });
